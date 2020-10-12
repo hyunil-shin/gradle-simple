e561e2e92b5c2b2b4b3b2c92084452008c5b38ec
Hyunil Shin <hyunil.shin@nhn.com>
2020-10-12 21:51:52 +0900

    update

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,10 +1,16 @@
+import com.offbytwo.jenkins.JenkinsServer;
+import java.net.URI;
+
 
 public class Hello {
   
   public static String GREETING = "Hello world!";
   
-  public static void main(String []args) {
-    
+  public static void main(String []args) throws Exception {
+
+
+	JenkinsServer j = new JenkinsServer(new URI("http://133.186.221.15:8080/"), "admin", "11d477c26344c0674c9ce91fb332953ee0");
+
   }  
 }
 
